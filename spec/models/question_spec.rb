87045b315572fc038e81daa1ac47cb15fce0c8c4
thomasjacobjr <thomasjacobjr@gmail.com>
2016-01-13 09:35:44 -0700

    Assignment 28 Rails-Models: Completed

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  let(:question) { Question.new(title: "New Question Body", body: "New Question Body", resolved: false)}
+
+  it "should respond to title" do
+    expect(question).to respond_to(:title)
+  end
+end
